5947f4ffccdbd67e9be3ff7b51c451c977410e15
Bhavya Poshiya <bhavyaposhiya@gmail.com>
2026-04-09 16:42:54 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+const BOOKS = [
+  {
+    id: "book1",
+    title: "Akram Express April",
+    category: "Stories",
+    cover: "books/book1/page1.jpg",
+    pages: 16
+  },
+  {
+    id: "book2",
+    title: "Akram Express March",
+    category: "Stories",
+    cover: "books/book2/page1.jpg",
+    pages: 16
+  },
+  {
+    id: "book3",
+    title: "Moral Lessons",
+    category: "Education",
+    cover: "books/book3/page1.jpg",
+    pages: 12
+  }
+];
